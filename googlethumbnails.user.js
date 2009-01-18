12e9162acb66ad030eb883987c83a922386eb3cc
niw <niw@377c1d59-e109-0410-9795-f258d49c5719>
2009-01-18 15:52:24 +0000

    * FIX: follow changes on google  * use googlepreview.com
    git-svn-id: svn+ssh://svn.disense.org/var/svn/niw/userscripts@1540 377c1d59-e109-0410-9795-f258d49c5719

--- googlethumbnails.user.js
+++ googlethumbnails.user.js
@@ -24,13 +24,14 @@ try {
 		}
 		return result;
 	}
-	array_each(xpath("//*[(name() = 'li' and @class='g')]", document), function(result) {
+	array_each(xpath("//*[((name() = 'LI' or name() = 'li') and contains(@class,'g'))]", document), function(result) {
 		var result_link = xpath("./h3/a", result)[0];
 		if(result_link) {
 			result.setAttribute("style", result.getAttribute("style") + "; clear: left;");
 			var result_url = result_link.getAttribute("href");
 			var real_result_url = result_url.replace(/^(?:([^\/].*)|\/url\?.*q=([^&]+)&?.*$)/i, "$1$2");
-			var thumb_url = "http://open.thumbshots.org/image.pxf?url=" + real_result_url;
+			var letter = real_result_url.replace(/^[^:]*:\/\/(?:www\.)?(.).*/, "$1");
+			var thumb_url = "http://" + letter + ".googlepreview.com/preview?s=" + real_result_url;
 
 			var thumbnail = document.createElement("div");
 			thumbnail.setAttribute("style", "float: left; margin: 0 8px 2px 0; width: 110px; height: 80px; overflow: hidden; background: #D5DDF3; border: 1px solid #3366CC;");
