6a97423e31ae9a9b1c9286525442c426d06301fc
Yoshimasa Niwa <niw@niw.at>
2009-09-11 21:13:49 +0900

    Support AutoPagerize

--- googlethumbnails.user.js
+++ googlethumbnails.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Google Thumbnails for Safari
-// @version       0.2
+// @version       0.2.1
 // @namespace     http://niw.at/
 // @description   Adds web site thumbnail images to google search results, original is from http://flet.ch/things/greasemonkey/
 // @include       http://www.google.*/search*
@@ -24,21 +24,28 @@ try {
 		}
 		return result;
 	}
-	array_each(xpath("//*[((name() = 'LI' or name() = 'li') and contains(@class,'g'))]", document), function(result) {
-		var result_link = xpath("./h3/a", result)[0];
-		if(result_link) {
-			result.setAttribute("style", result.getAttribute("style") + "; clear: left;");
-			var result_url = result_link.getAttribute("href");
-			var real_result_url = result_url.replace(/^(?:([^\/].*)|\/url\?.*q=([^&]+)&?.*$)/i, "$1$2");
-			var letter = real_result_url.replace(/^[^:]*:\/\/(?:www\.)?(.).*/, "$1");
-			var thumb_url = "http://" + letter + ".googlepreview.com/preview?s=" + real_result_url;
+	function addThumbnails(target) {
+		array_each(xpath(".//*[((name() = 'LI' or name() = 'li') and contains(@class,'g'))]", target), function(result) {
+			var result_link = xpath("./h3/a", result)[0];
+			if(result_link) {
+				result.setAttribute("style", result.getAttribute("style") + "; clear: left;");
+				var result_url = result_link.getAttribute("href");
+				var real_result_url = result_url.replace(/^(?:([^\/].*)|\/url\?.*q=([^&]+)&?.*$)/i, "$1$2");
+				var letter = real_result_url.replace(/^[^:]*:\/\/(?:www\.)?(.).*/, "$1");
+				var thumb_url = "http://" + letter + ".googlepreview.com/preview?s=" + real_result_url;
 
-			var thumbnail = document.createElement("div");
-			thumbnail.setAttribute("style", "float: left; margin: 0 8px 2px 0; width: 110px; height: 80px; overflow: hidden; background: #D5DDF3; border: 1px solid #3366CC;");
-			thumbnail.innerHTML = '<a href="' + result_url + '"><img src="' + thumb_url + '" align="left" height="80" width="110" /></a>';
-			insertNext(result_link, thumbnail);
-		}
-	});
+				var thumbnail = document.createElement("div");
+				thumbnail.setAttribute("style", "float: left; margin: 0 8px 2px 0; width: 110px; height: 80px; overflow: hidden; background: #D5DDF3; border: 1px solid #3366CC;");
+				thumbnail.innerHTML = '<a href="' + result_url + '"><img src="' + thumb_url + '" align="left" height="80" width="110" /></a>';
+				insertNext(result_link, thumbnail);
+			}
+		});
+	}
+	document.body.addEventListener("AutoPagerize_DOMNodeInserted", function(e){
+		var node = e.target;
+		addThumbnails(node);
+	}, false);
+	addThumbnails(document);
 } catch(exception) {
 	var tag = document.createElement("div");
 	tag.setAttribute("style", "padding: 4px; font-size: 10px; color: #fff; border: 1px solid #f00; background: #c00;");
