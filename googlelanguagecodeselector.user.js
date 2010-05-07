cde73db87259e1119aae9ea61b30b689acea3b8d
Yoshimasa Niwa <niw@niw.at>
2010-05-06 23:26:27 -0700

    Bump version up to 0.3.0

--- googlelanguagecodeselector.user.js
+++ googlelanguagecodeselector.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Google Language Code Selector
-// @version        0.2.1
+// @version        0.3.0
 // @namespace      http://niw.at/
 // @description    Select hl paramter for google query
 // @include        http://www.google.*/*q=*
